78c4de6afa6be6147022cec278182cb94eb8c36a
Neil <neilkhess@protonmail.com>
2023-02-18 11:31:26 -0800

    Made more progress on the div construction.  Trying to find a solution to create and append multiple divs.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
+let sideLen = function returnText(){document.getElementById("sideLenText").value;
+}
+
+
 const gridArea = document.querySelector('#gridArea'); 
 
 const divBox = document.createElement("div");
-divBox.innerHTML = "0";
+divBox.innerHTML = "X";
+divBox.style.width = "10px";
+divBox.style.height = "10px";
+divBox.style.background = "green";
+divBox.style.margin = "5px";
+divBox.style.display = "inline-block";
 divBox.classList.add("oneSquare");
 
-let divGen = function(sideLen){
-    for (i = 0; i < sideLen; i++) {gridArea.appendChild(divBox)}
-}
 
-divGen(5);
+let divGen = function(sideLen){
+    for (i = 0; i < sideLen; i++) gridArea.appendChild(divBox)}
+
+divGen(4);
