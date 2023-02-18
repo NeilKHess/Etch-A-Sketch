47b4bdeb920b3d780daec89b9a911d9dbc5695ed
Neil Hess <neilkhess@protonmail.com>
2023-02-18 14:39:57 -0800

    Still working on line breaks for the DIVS

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
 const gridArea = document.querySelector('#gridArea'); 
+const gridBreak = document.createElement("br");
+sideLen = "3"
 
-const divBox = document.createElement("div");
-divBox.innerHTML = "X";
-divBox.style.width = "10px";
-divBox.style.height = "10px";
-divBox.style.background = "green";
-divBox.style.margin = "5px";
-divBox.style.display = "inline-block";
-divBox.classList.add("oneSquare");
-
-
-let divGen = function(){
-    for (i = 0; i < 10; i++) gridArea.append(divBox)}
-
-divGen(9);
+let divGen = function(sideLen)
+    {for (j = 0; j < sideLen; j++)  
+        for (i = 0; i < sideLen; i++) {
+            const divBox = document.createElement("div");
+                divBox.innerHTML = "<div>O</div>";
+                divBox.style.width = "15px";
+                divBox.style.height = "15px";
+                divBox.style.background = "limegreen";
+                divBox.style.margin = "2px";
+                divBox.classList.add("oneSquare");
+                    gridArea.append(divBox);}  
+                    };
+divGen(sideLen);
