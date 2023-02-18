82ff47748586049d215f191db0865b30010bd97f
Neil Hess <neilkhess@protonmail.com>
2023-02-18 09:35:12 -0800

    Made progress on adding divs in teh DOM.  Need to figure out why my for loop isn't acting as expected

--- scripts.js
+++ scripts.js
@@ -1,40 +1,11 @@
-//(Remember there is going to be a GENERATE button to make this happen)
-//[ENTER SIDE LENGTH]
+const gridArea = document.querySelector('#gridArea'); 
 
-//[GENERATE BUTTON CLICK]
-//For loop to push number of spaces into "innards" array
-let innards = [];
+const divBox = document.createElement("div");
+divBox.innerHTML = "0";
+divBox.classList.add("oneSquare");
 
-//Make Side length input box
-
-let spacePush = function(sideLen){
-    for (i = 0; i < sideLen; i++) {innards.push('_')}
+let divGen = function(sideLen){
+    for (i = 0; i < sideLen; i++) {gridArea.appendChild(divBox)}
 }
 
-
-
-
-//Make click listener that 1) retrives value 2) generates the boxes 3) alerts the user to try another value if it is NOT between 1-99
-
-spacePush(10);
-
-
-
-//construction and delivery of "boxes"
-let gridCanvas = document.querySelector('#gridCanvas');
-
-
-
-let boxes = innards.map(innards => {
-    let li = document.createElement('li');
-    li.textContent = innards;
-    return li;
-});
-
-
-//Make click listener
-gridCanvas.append(...boxes);
-
-//Helpful Videos
-//appendChild() tutorial: https://www.youtube.com/watch?v=e0ihEHxd6vI
-//https://www.javascripttutorial.net/javascript-dom/javascript-append/
+divGen(5);
