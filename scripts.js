82aa047bbe92a7d57938617855a187e309195485
Neil <neilkhess@protonmail.com>
2023-02-19 11:13:37 -0800

    Code for getting user input completed.  Big progress made today.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,23 @@
 const gridArea = document.querySelector('#gridArea'); 
 const gridBreak = document.createElement("br");
-sideLen = "3"
+const genButton = document.querySelector("#generateButton");
 
-let divGen = function(sideLen)
+genButton.addEventListener("click", function(){
+    let sideLenPrompt = prompt("How large would you like the sides of your square to be?")
+    let sideLen = sideLenPrompt;
+    let divGen = function(sideLen)
     {for (j = 0; j < sideLen; j++)  
         for (i = 0; i < sideLen; i++) {
             const divBox = document.createElement("div");
-                divBox.innerHTML = "<div>O</div>";
+                divBox.innerHTML = "<div> </div>";
                 divBox.style.width = "15px";
                 divBox.style.height = "15px";
                 divBox.style.background = "limegreen";
-                divBox.style.margin = "2px";
+                divBox.style.margin = "0px";
                 divBox.classList.add("oneSquare");
                 divBox.addEventListener("mouseenter",function() {divBox.classList.add("revealed")})
                 divBox.addEventListener("mouseout",function() {divBox.classList.remove("revealed")})
                     gridArea.append(divBox);}  
                     };
-divGen(sideLen);
+    divGen(sideLen);
+})
