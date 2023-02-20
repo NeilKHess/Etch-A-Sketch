849dfa4ce3b7a0a629acae450963fced1f17e08c
Neil <neilkhess@protonmail.com>
2023-02-20 10:36:01 -0800

    Created simple grid.  Creating experimental branch for various game modes.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
+//Location references
 const gridArea = document.querySelector('#gridArea'); 
-const gridBreak = document.createElement("br");
 const genButton = document.querySelector("#generateButton");
 
+//Game Modes//
+// const defaultMode = funciton();
+// const emojiMode = funciton();
+// const scrabbleMode = funciton();
+// const scratcherMode = funciton();
+// const eraserMode = funciton();
+// const customTextMode = funciton();
+// const whackAMode = function();
+// const myTeamMode = function();
+
+//Style Selectors//
+// const gridAreaColor = function();
+// const gridSquareColor = function();
+
+
+
+//Cycle Begins here
 genButton.addEventListener("click", function(){
 
     //Clears results of any previous grid//
@@ -12,7 +29,7 @@ genButton.addEventListener("click", function(){
     const gridArea = document.querySelector('#gridArea');
     removeAllChildNodes(gridArea);
 
-    //Grid Generator
+    //Default Mode Generator
     {let sideLenPrompt = prompt("How large would you like the sides of your square to be? (Max value = 100)")
     let sideLen = sideLenPrompt;
     let divGen = function(sideLen){
@@ -22,13 +39,25 @@ genButton.addEventListener("click", function(){
                 gridArea.style.gridTemplateColumns = `repeat(${sideLen}, 1fr)`;
                 gridArea.style.gridTemplateRows = `repeat(${sideLen}, 1fr)`;
                 divBox.innerHTML = "<div> </div>";
-                divBox.style.background = "limegreen";
+                divBox.style.background = "orange";
                 divBox.style.margin = "0px";
                 divBox.classList.add("oneSquare");
-                divBox.addEventListener("mouseenter",function() {divBox.classList.add("revealed")})
-                divBox.addEventListener("mouseout",function() {divBox.classList.remove("revealed")})
+                divBox.addEventListener("mouseenter",function() {divBox.classList.add("defaultModeRevealed")})
+                divBox.addEventListener("mouseout",function() {divBox.classList.remove("defaultModeRevealed")})
                 gridArea.append(divBox);}  
                 };
     divGen(sideLen);
 }})
 
+
+
+//FEATURES TO ADD TO THIS//
+//Custom Text (CRUD functionality)
+//Obhect oriented modes
+//Make the "modes" load from JSON settings?
+//Emoji mode
+//Custom thumbnails
+//Scrabble Mode (Each box reveals a random letter)
+//Scratcher mode (gradually uncover opacity 10% with a gray scrather film, change mouse cursor to a "penny" to scratch, make a single square say "winner")
+//Eraser mode (gradually erase a full grid 10%)
+//Slider? https://www.w3schools.com/jsref/prop_range_value.asp
